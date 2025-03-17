e4d2a7eb50be1773d0813bc0ac98ff031469844d
182-Skylane <skylane.mail@gmail.com>
2025-03-17 20:43:59 +0700

    before hw

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import { updateComments } from './comments.js'
 import { initAddCommentListener } from './initListeners.js'
 import { renderComments } from './renderComments.js'
 
+document.querySelector('.comments').innerHTML =
+    'Пожалуйста подождите, загружаю комментарии...'
+
 fetchComments().then((data) => {
     updateComments(data)
     renderComments()
