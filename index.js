00238fd2b5be13f9b215544eeef7de957b76a9a6
182-Skylane <skylane.mail@gmail.com>
2025-03-18 21:18:30 +0700

    add-form not seen before renderComments

--- index.js
+++ index.js
@@ -6,9 +6,12 @@ import { renderComments } from './renderComments.js'
 document.querySelector('.comments').innerHTML =
     'Пожалуйста подождите, загружаю комментарии...'
 
+
 fetchComments().then((data) => {
     updateComments(data)
     renderComments()
+    
+document.querySelector('.add-form').style.display = 'flex'
 })
 
 initAddCommentListener(renderComments)
